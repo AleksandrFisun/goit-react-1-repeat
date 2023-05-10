427579125c7fe9c90ff07eb51a2dd37a0ad0ef95
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2023-05-10 12:21:16 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/AleksandrFisun/goit-react-1-repeat/">
+    <BrowserRouter basename="/aleksandrfisun.github.io/AleksandrFisun/goit-react-1-repeat/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
